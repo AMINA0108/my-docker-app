8791dd501b7b9cd5015e65deca91d3b48548b496
Amina <batjargalamina135@gmail.com>
2026-05-14 09:57:46 +0000

    Update version to 2.0.0

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def home():
     return jsonify({
         "message": "Миний custom Docker image!",
         "author": "aamiii",
-        "version": "1.0.0"
+        "version": "2.0.0"
     })
 
 @app.route("/health")
